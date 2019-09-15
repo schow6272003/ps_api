25b6961881e5b8c1e07bc62a909ee536a7e02f89
steven.chow <stevenwchow@gmail.com>
2019-09-15 16:17:41 -0700

    fix reference error on heroku deployment

--- app.js
+++ app.js
@@ -31,23 +31,9 @@ app.use(function(req, res, next) {
 });
 
 
-app.get("/", function(req,res){
-    client.on("error", function (err) {
-        console.log("Error " + err);
-    });
-     
-    client.set("string key", "string val", redis.print);
-    client.hset("hash key", "hashtest 1", "some value", redis.print);
-    client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-    client.hkeys("hash key", function (err, replies) {
-        console.log(replies.length + " replies:");
-        replies.forEach(function (reply, i) {
-            console.log("    " + i + ": " + reply);
-        });
-        client.quit();
-    });
+// app.get("/", function(req,res){
 
-});
+// });
 
 app.use('/api', CBSAV1);
 
